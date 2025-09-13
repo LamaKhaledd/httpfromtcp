8ca1f24e7d43da429165f8a63ae30d01e56e2261
LamaKhaledd <s12027995@stu.najah.edu>
2025-09-13 14:51:49 +0300

    Refactor reading lines into reusable channel function

--- main.go
+++ main.go
@@ -11,40 +11,54 @@ import (
 
 const inputFilePath = "messages.txt"
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer f.Close()
+		var currentLine string
+		buffer := make([]byte, 8)
+
+		for {
+			n, err := f.Read(buffer)
+			if err != nil {
+				if errors.Is(err, io.EOF) {
+					break
+				}
+				log.Printf("error reading file: %s\n", err)
+				break
+			}
+
+			chunk := string(buffer[:n])
+			parts := strings.Split(chunk, "\n")
+
+			for i, part := range parts {
+				if i < len(parts)-1 {
+					ch <- currentLine + part
+					currentLine = ""
+				} else {
+					currentLine += part
+				}
+			}
+		}
+
+		if len(currentLine) > 0 {
+			ch <- currentLine
+		}
+
+		close(ch)
+	}()
+
+	return ch
+}
+
 func main() {
 	f, err := os.Open(inputFilePath)
 	if err != nil {
 		log.Fatalf("could not open %s: %s\n", inputFilePath, err)
 	}
-	defer f.Close()
 
-	var currentLine string
-
-	for {
-		b := make([]byte, 8)
-		n, err := f.Read(b)
-		if err != nil {
-			if errors.Is(err, io.EOF) {
-				break
-			}
-			fmt.Printf("error: %s\n", err.Error())
-			return
-		}
-
-		chunk := string(b[:n])
-		parts := strings.Split(chunk, "\n")
-
-		for i, part := range parts {
-			if i < len(parts)-1 {
-				fmt.Printf("read: %s\n", currentLine+part)
-				currentLine = ""
-			} else {
-				currentLine += part
-			}
-		}
-	}
-
-	if len(currentLine) > 0 {
-		fmt.Printf("read: %s\n", currentLine)
+	for line := range getLinesChannel(f) {
+		fmt.Printf("read: %s\n", line)
 	}
 }
